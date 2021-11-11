5d9557ffac78d1b06cc968eb3c594a892514b16a
Niall Siegenheim <sniall@student.ethz.ch>
2021-11-11 14:11:08 +0100

    attempt for communication during timestep (doesn't work)

--- polybench-c-4.2.1-beta/stencils/jacobi-2d/jacobi-2d.c
+++ polybench-c-4.2.1-beta/stencils/jacobi-2d/jacobi-2d.c
@@ -22,20 +22,20 @@
 #include "jacobi-2d.h"
 
 /* Array initialization. */
-static void init_array(int n, DATA_TYPE POLYBENCH_2D(A, N, N, n, n),
-                       DATA_TYPE POLYBENCH_2D(B, N, N, n, n)) {
+static void init_array(int n, double* A, double* B) {
   int i, j;
 
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      A[i][j] = ((DATA_TYPE)i * (j + 2) + 2) / n;
-      B[i][j] = ((DATA_TYPE)i * (j + 3) + 3) / n;
+      // A[n * i + j] = ((DATA_TYPE)i * (j + 2) + 2) / n;
+      A[n * i + j] = 1;
+      B[n * i + j] = ((DATA_TYPE)i * (j + 3) + 3) / n;
     }
 }
 
 /* DCE code. Must scan the entire live-out data.
    Can be used also to check the correctness of the output. */
-static void print_array(int n, DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
+static void print_array(int n, double* A)
 
 {
   int i, j;
@@ -44,34 +44,107 @@ static void print_array(int n, DATA_TYPE POLYBENCH_2D(A, N, N, n, n))
   POLYBENCH_DUMP_BEGIN("A");
   for (i = 0; i < n; i++)
     for (j = 0; j < n; j++) {
-      if ((i * n + j) % 20 == 0) fprintf(POLYBENCH_DUMP_TARGET, "\n");
-      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, A[i][j]);
+      if (j == 0) fprintf(POLYBENCH_DUMP_TARGET, "\n");
+      fprintf(POLYBENCH_DUMP_TARGET, DATA_PRINTF_MODIFIER, A[n * i + j]);
     }
   POLYBENCH_DUMP_END("A");
   POLYBENCH_DUMP_FINISH;
 }
 
+static void exchange_cells(double* A, int nx_local, int ny_local,
+                           int* neighbours, MPI_Comm* comm_cart,
+                           MPI_Datatype* column_vec) {
+  MPI_Request requests[8];
+  for (int i = 0; i < 8; i++) requests[i] = MPI_REQUEST_NULL;
+
+  /* Communication with top neighbour */
+  if (neighbours[0] != MPI_PROC_NULL) {
+    MPI_Isend(&A[nx_local + 3], nx_local, MPI_DOUBLE, neighbours[0], 0,
+              *comm_cart, &requests[0]);
+    MPI_Irecv(&A[1], nx_local, MPI_DOUBLE, neighbours[0], MPI_ANY_TAG,
+              *comm_cart, &requests[1]);
+  } else {
+    requests[0] = MPI_REQUEST_NULL;
+    requests[1] = MPI_REQUEST_NULL;
+  }
+
+  /* Communication with bottom neighbour */
+  if (neighbours[1] != MPI_PROC_NULL) {
+    MPI_Isend(&A[(nx_local + 2) * (ny_local + 2) - 2 * (nx_local + 2) + 1],
+              nx_local, MPI_DOUBLE, neighbours[1], 0, *comm_cart, &requests[2]);
+    MPI_Irecv(&A[(nx_local + 2) * (ny_local + 2) - nx_local - 1], nx_local,
+              MPI_DOUBLE, neighbours[1], MPI_ANY_TAG, *comm_cart, &requests[3]);
+  } else {
+    requests[2] = MPI_REQUEST_NULL;
+    requests[3] = MPI_REQUEST_NULL;
+  }
+
+  /* Communication with left neighbour */
+  if (neighbours[2] != MPI_PROC_NULL) {
+    MPI_Isend(&A[nx_local + 3], 1, *column_vec, neighbours[2], 0, *comm_cart,
+              &requests[4]);
+    MPI_Irecv(&A[nx_local + 2], 1, *column_vec, neighbours[2], MPI_ANY_TAG,
+              *comm_cart, &requests[5]);
+  } else {
+    requests[4] = MPI_REQUEST_NULL;
+    requests[5] = MPI_REQUEST_NULL;
+  }
+
+  /* Communication with right neighbour */
+  if (neighbours[3] != MPI_PROC_NULL) {
+    MPI_Isend(&A[2 * (nx_local + 2) - 2], 1, *column_vec, neighbours[3], 0,
+              *comm_cart, &requests[6]);
+    MPI_Irecv(&A[2 * (nx_local + 2) - 1], 1, *column_vec, neighbours[3],
+              MPI_ANY_TAG, *comm_cart, &requests[7]);
+  } else {
+    requests[6] = MPI_REQUEST_NULL;
+    requests[7] = MPI_REQUEST_NULL;
+  }
+
+  /* Wait for all communication to finish */
+  MPI_Waitall(8, requests, MPI_STATUSES_IGNORE);
+}
+
 /* Main computational kernel. The whole function will be timed,
    including the call and return. */
-static void kernel_jacobi_2d(int tsteps, int nx, int ny,
-                             DATA_TYPE POLYBENCH_2D(A, nx, ny, nx, nx),
-                             DATA_TYPE POLYBENCH_2D(B, nx, ny, nx, ny)) {
+static void kernel_jacobi_2d(int tsteps, int nx_local, int ny_local, double* A,
+                             double* B, int* neighbours, MPI_Comm* comm_cart,
+                             MPI_Datatype* column_vec) {
   int t, i, j;
 
-#pragma scop
-  /* TODO: Exchange ghost cells */
+  /* Define bounds for iteration bounds, depending on location of processor in
+   * the cartesian grid */
+  int x_bound_low = 1;
+  int x_bound_high = nx_local + 1;
+  int y_bound_low = 1;
+  int y_bound_high = ny_local + 1;
+
+  if (neighbours[0] == MPI_PROC_NULL) y_bound_low += 1;
+  if (neighbours[1] == MPI_PROC_NULL) y_bound_high -= 1;
+  if (neighbours[2] == MPI_PROC_NULL) x_bound_low += 1;
+  if (neighbours[3] == MPI_PROC_NULL) x_bound_high -= 1;
 
   for (t = 0; t < _PB_TSTEPS; t++) {
-    for (i = 1; i < nx - 1; i++)
-      for (j = 1; j < ny - 1; j++)
-        B[i][j] = SCALAR_VAL(0.2) * (A[i][j] + A[i][j - 1] + A[i][1 + j] +
-                                     A[1 + i][j] + A[i - 1][j]);
-    for (i = 1; i < nx - 1; i++)
-      for (j = 1; j < ny - 1; j++)
-        A[i][j] = SCALAR_VAL(0.2) * (B[i][j] + B[i][j - 1] + B[i][1 + j] +
-                                     B[1 + i][j] + B[i - 1][j]);
+    exchange_cells(A, nx_local, ny_local, neighbours, comm_cart, column_vec);
+    //   /* TODO: Implement SIMD instructions */
+    for (i = x_bound_low; i < x_bound_high; i++)
+      for (j = y_bound_low; j < y_bound_high; j++)
+        B[(nx_local + 2) * i + j] =
+            SCALAR_VAL(0.2) *
+            (A[(nx_local + 2) * i + j] + A[(nx_local + 2) * i + j - 1] +
+             A[(nx_local + 2) * i + 1 + j] + A[(nx_local + 2) * (1 + i) + j] +
+             A[(nx_local + 2) * (i - 1) + j]);
+
+    exchange_cells(B, nx_local, ny_local, neighbours, comm_cart, column_vec);
+
+    for (i = x_bound_low; i < x_bound_high; i++)
+      for (j = y_bound_low; j < y_bound_high; j++)
+        A[(nx_local + 2) * i + j] =
+            SCALAR_VAL(0.2) *
+            (B[(nx_local + 2) * i + j] + B[(nx_local + 2) * i + j - 1] +
+             B[(nx_local + 2) * i + 1 + j] + B[(nx_local + 2) * (1 + i) + j] +
+             B[(nx_local + 2) * (i - 1) + j]);
   }
-#pragma endscop
 }
 
 int main(int argc, char** argv) {
@@ -92,9 +165,9 @@ int main(int argc, char** argv) {
   MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &comm_cart);
 
   /* Find neighbours of each rank */
-  int rank_top, rank_bottom, rank_left, rank_right;
-  MPI_Cart_shift(comm_cart, 0, 1, &rank_top, &rank_bottom);
-  MPI_Cart_shift(comm_cart, 1, 1, &rank_left, &rank_right);
+  int neighbours[4];  // top, bottom, left, right
+  MPI_Cart_shift(comm_cart, 0, 1, &neighbours[0], &neighbours[1]);
+  MPI_Cart_shift(comm_cart, 1, 1, &neighbours[2], &neighbours[3]);
 
   /* Retrieve problem size. */
   int n = N;
@@ -102,37 +175,53 @@ int main(int argc, char** argv) {
 
   /* Calculate problem size of local domain for every rank */
   int nx_local = n / dims[0];
-  if (rank_right == MPI_PROC_NULL) nx_local += n % dims[0];
+  if (neighbours[3] == MPI_PROC_NULL) nx_local += n % dims[0];
 
   int ny_local = n / dims[1];
-  if (rank_bottom == MPI_PROC_NULL) ny_local += n % dims[1];
+  if (neighbours[1] == MPI_PROC_NULL) ny_local += n % dims[1];
 
-  /* TODO: Define a data type for sending columns of local domains */
+  /* Define a data type for sending columns of local domains */
+  MPI_Datatype column_vec;
+  MPI_Type_vector(ny_local, 1, nx_local + 2, MPI_DOUBLE, &column_vec);
+  MPI_Type_commit(&column_vec);
 
   int sendcounts[size];
   int senddispls[size];
-  MPI_Datatype blocktypes[dims[0] * dims[1]];
+  MPI_Datatype blocktypes[size];
   int recvcounts[size];
   int recvdispls[size];
   MPI_Datatype recvtypes[size];
 
+  /* Define a data type for each rank's subdomain */
+  MPI_Datatype blocktype;
+  MPI_Type_vector(ny_local, nx_local, nx_local + 2, MPI_DOUBLE, &blocktype);
+  MPI_Type_commit(&blocktype);
+
   for (int proc = 0; proc < size; proc++) {
     recvcounts[proc] = 0;
-    recvdispls[proc] = nx_local + 3;
-    recvtypes[proc] = MPI_DOUBLE;
+    recvdispls[proc] = (nx_local + 3) * sizeof(double);
+    recvtypes[proc] = blocktype;
 
     sendcounts[proc] = 0;
-    senddispls[proc] = 0;
+    senddispls[proc] = (nx_local + 3) * sizeof(double);
   }
-  recvcounts[0] = nx_local * ny_local;
+  recvcounts[0] = 1;
+
+  double* A;
+  double* B;
 
   if (rank == 0) {
     /* Variable declaration/allocation. */
-    POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
-    POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
+    // POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, N, N, n, n);
+    // POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, N, N, n, n);
+
+    A = (double*)malloc(n * n * sizeof(double));
+    B = (double*)malloc(n * n * sizeof(double));
 
     /* Initialize array(s). */
-    init_array(n, POLYBENCH_ARRAY(A), POLYBENCH_ARRAY(B));
+    init_array(n, A, B);
+
+    print_array(n, A);
 
     /* TODO: Define a data type for sending parts of the matrix back and forth
      */
@@ -195,61 +284,62 @@ int main(int argc, char** argv) {
     }
 
     printf("finished displ calc\n");
+  }
 
-    POLYBENCH_2D_ARRAY_DECL(A_local, DATA_TYPE, nx_local + 2, ny_local + 2,
-                            nx_local + 2, ny_local + 2);
-    POLYBENCH_2D_ARRAY_DECL(B_local, DATA_TYPE, nx_local + 2, ny_local + 2,
-                            nx_local + 2, ny_local + 2);
+  double* A_local =
+      (double*)malloc((nx_local + 2) * (ny_local + 2) * sizeof(double));
+  double* B_local =
+      (double*)malloc((nx_local + 2) * (ny_local + 2) * sizeof(double));
 
-    printf("declared arrays\n");
+  /* Send the segregated domain to all other ranks */
+  MPI_Alltoallw(A, sendcounts, senddispls, blocktypes, A_local, recvcounts,
+                recvdispls, recvtypes, MPI_COMM_WORLD);
+  MPI_Alltoallw(B, sendcounts, senddispls, blocktypes, B_local, recvcounts,
+                recvdispls, recvtypes, MPI_COMM_WORLD);
 
-    /* TODO: Send the segregated domain to all other ranks */
-    MPI_Alltoallw(A, sendcounts, senddispls, blocktypes, A_local, recvcounts,
-                  recvdispls, recvtypes, MPI_COMM_WORLD);
+  if (rank == 0) {
+    print_array(nx_local + 2, A_local);
+    print_array(nx_local + 2, B_local);
+  }
 
-    printf("performed alltoall\n");
-
-    /* Run kernel. */
-    kernel_jacobi_2d(tsteps, nx_local, ny_local, POLYBENCH_ARRAY(A_local),
-                     POLYBENCH_ARRAY(B_local));
-
-    printf("finished kernel\n");
-
-    /* Stop and print timer. */
+  if (rank == 0) {
     /* Start timer. */
     polybench_start_instruments;
+  }
+
+  /* Run kernel. */
+  kernel_jacobi_2d(tsteps, nx_local, ny_local, A_local, B_local, neighbours,
+                   &comm_cart, &column_vec);
+
+  if (rank == 0) {
+    /* Stop and print timer. */
     polybench_stop_instruments;
     polybench_print_instruments;
-
-    /* Prevent dead-code elimination. All live-out data must be printed
-       by the function call in argument. */
-    polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(A)));
-
-    /* Be clean. */
-    POLYBENCH_FREE_ARRAY(A);
-    POLYBENCH_FREE_ARRAY(B);
-    POLYBENCH_FREE_ARRAY(A_local);
-    POLYBENCH_FREE_ARRAY(B_local);
-  } else {
-    POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, nx_local + 2, ny_local + 2,
-                            nx_local + 2, ny_local + 2);
-    POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, nx_local + 2, ny_local + 2,
-                            nx_local + 2, ny_local + 2);
-
-    MPI_Alltoallw(A, sendcounts, senddispls, blocktypes, A, recvcounts,
-                  recvdispls, recvtypes, MPI_COMM_WORLD);
-
-    /* Run kernel. */
-    kernel_jacobi_2d(tsteps, nx_local, ny_local, POLYBENCH_ARRAY(A),
-                     POLYBENCH_ARRAY(B));
-
-    // /* TODO: Send local domain back to main rank */
-    // MPI_Gatherv();
-    // MPI_Gatherv();
-
-    POLYBENCH_FREE_ARRAY(A);
-    POLYBENCH_FREE_ARRAY(B);
   }
+
+  if (rank == 0) {
+    print_array(nx_local + 2, A_local);
+    print_array(nx_local + 2, B_local);
+  }
+
+  /* TODO: Put submatrices back together */
+  MPI_Alltoallw(A_local, recvcounts, recvdispls, recvtypes, A, sendcounts,
+                senddispls, blocktypes, comm_cart);
+
+  /* Prevent dead-code elimination. All live-out data must be printed
+     by the function call in argument. */
+  // polybench_prevent_dce(print_array(n, POLYBENCH_ARRAY(A)));
+
+  if (rank == 0) print_array(n, A);
+
+  /* Be clean. */
+  if (rank == 0) {
+    free(A);
+    free(B);
+  }
+
+  free(A_local);
+  free(B_local);
 
   MPI_Finalize();
 
