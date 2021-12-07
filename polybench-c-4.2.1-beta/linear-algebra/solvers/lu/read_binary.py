487316f4e384be38072654d2c3fd300840a1e762
Niall Siegenheim <sniall@student.ethz.ch>
2021-12-07 14:08:15 +0100

    python script update

--- polybench-c-4.2.1-beta/linear-algebra/solvers/lu/read_binary.py
+++ polybench-c-4.2.1-beta/linear-algebra/solvers/lu/read_binary.py
@@ -1,7 +1,20 @@
 import struct
 from functools import partial
+import numpy as np
+import sys
+
+np.set_printoptions(threshold=sys.maxsize)
+
+n = 40
 
 f = open('lu.out', encoding='latin1')
 
-for chunk in iter(partial(f.read, 8), b''):
-    print(struct.unpack('d', bytes(chunk, encoding='latin1')))
+elements = []
+
+for chunk, i in zip(iter(partial(f.read, 8), b''), range(n**2)):
+    elements.append(struct.unpack('d', bytes(chunk, encoding='latin1')))
+
+matrix = np.matrix(elements)
+matrix = matrix.reshape(n, n)
+
+print(matrix)
