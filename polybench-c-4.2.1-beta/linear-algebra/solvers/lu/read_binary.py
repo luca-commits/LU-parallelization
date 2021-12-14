d1b3e15e6c8fcc0df8cbf4f9c52771ba4f27f81a
Niall Siegenheim <sniall@student.ethz.ch>
2021-12-14 15:29:05 +0100

    python script for verification

--- polybench-c-4.2.1-beta/linear-algebra/solvers/lu/read_binary.py
+++ polybench-c-4.2.1-beta/linear-algebra/solvers/lu/read_binary.py
@@ -1,11 +1,23 @@
 import struct
 from functools import partial
 import numpy as np
+import scipy.linalg
 import sys
 
 np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
 n = 40
+
+f = open('lu_init.out', encoding='latin1')
+
+elements = []
+
+for chunk, i in zip(iter(partial(f.read, 8), b''), range(n**2)):
+    elements.append(struct.unpack('d', bytes(chunk, encoding='latin1')))
+
+matrix_init = np.matrix(elements)
+matrix_init = matrix.reshape(n, n)
+
 
 f = open('lu.out', encoding='latin1')
 
@@ -17,6 +29,8 @@ for chunk, i in zip(iter(partial(f.read, 8), b''), range(n**2)):
 matrix = np.matrix(elements)
 matrix = matrix.reshape(n, n)
 
-print(matrix.shape)
+print(np.linalg.norm(matrix))
+
+
 
 print(matrix)
