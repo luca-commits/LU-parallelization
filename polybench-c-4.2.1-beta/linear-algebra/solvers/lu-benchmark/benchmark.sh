aa18163b3ed7dc1362db641f339a177208660434
Niall Siegenheim <sniall@student.ethz.ch>
2022-01-06 23:51:33 +0100

    hybrid benchmarking

--- polybench-c-4.2.1-beta/linear-algebra/solvers/lu-benchmark/benchmark.sh
+++ polybench-c-4.2.1-beta/linear-algebra/solvers/lu-benchmark/benchmark.sh
@@ -34,98 +34,159 @@ do
       cd ..
   fi
 
-  # if [ "$1" = "hybrid"]
-  # then
-  #   cd hybrid
+  if [ "$1" = "hybrid"]  || [ "$1" = "all" ]
+  then
+    cd hybrid
 
-  #   if [ "$ranks" -eq "1"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
-  #   elif [ "$ranks" -eq "2"]
-  #   then
-  #     mpi_ranks = 
-  #     omp_ranks = 
+    if [ "$ranks" -eq "1"]
+    then
+      mpi_ranks = 1
+      omp_ranks = 1
+    elif [ "$ranks" -eq "2"]
+    then
+      mpi_ranks = 2
+      omp_ranks = 1
+    elif [ "$ranks" -eq "3"]
+    then
+      mpi_ranks = 3
+      omp_ranks = 1
+    elif [ "$ranks" -eq "4"]
+    then
+      mpi_ranks = 2
+      omp_ranks = 2
+    elif [ "$ranks" -eq "5"]
+    then
+      mpi_ranks = 5
+      omp_ranks = 1
+    elif [ "$ranks" -eq "6"]
+    then
+      mpi_ranks = 3
+      omp_ranks = 2
+    elif [ "$ranks" -eq "7"]
+    then
+      mpi_ranks = 7
+      omp_ranks = 1
+    elif [ "$ranks" -eq "8"]
+    then
+      mpi_ranks = 4
+      omp_ranks = 2
+    elif [ "$ranks" -eq "9"]
+    then
+      mpi_ranks = 3
+      omp_ranks = 3
+    elif [ "$ranks" -eq "10"]
+    then
+      mpi_ranks = 5
+      omp_ranks = 2
+    elif [ "$ranks" -eq "11"]
+    then
+      mpi_ranks = 11
+      omp_ranks = 1
+    elif [ "$ranks" -eq "12"]
+    then
+      mpi_ranks = 4
+      omp_ranks = 3
+    elif [ "$ranks" -eq "13"]
+    then
+      mpi_ranks = 13
+      omp_ranks = 1
+    elif [ "$ranks" -eq "14"]
+    then
+      mpi_ranks = 7
+      omp_ranks = 2
+    elif [ "$ranks" -eq "15"]
+    then
+      mpi_ranks = 5
+      omp_ranks = 3
+    elif [ "$ranks" -eq "16"]
+    then
+      mpi_ranks = 4
+      omp_ranks = 4
+    elif [ "$ranks" -eq "17"]
+    then
+      mpi_ranks = 17
+      omp_ranks = 1
+    elif [ "$ranks" -eq "18"]
+    then
+      mpi_ranks = 6
+      omp_ranks = 3
+    elif [ "$ranks" -eq "19"]
+    then
+      mpi_ranks = 19
+      omp_ranks = 1
+    elif [ "$ranks" -eq "20"]
+    then
+      mpi_ranks = 5
+      omp_ranks = 4
+    elif [ "$ranks" -eq "21"]
+    then
+      mpi_ranks = 7
+      omp_ranks = 3
+    elif [ "$ranks" -eq "22"]
+    then
+      mpi_ranks = 11
+      omp_ranks = 2
+    elif [ "$ranks" -eq "23"]
+    then
+      mpi_ranks = 23
+      omp_ranks = 1
+    elif [ "$ranks" -eq "24"]
+    then
+      mpi_ranks = 6
+      omp_ranks = 4
+    elif [ "$ranks" -eq "25"]
+    then
+      mpi_ranks = 5
+      omp_ranks = 5
+    elif [ "$ranks" -eq "26"]
+    then
+      mpi_ranks = 13
+      omp_ranks = 2
+    elif [ "$ranks" -eq "27"]
+    then
+      mpi_ranks = 9
+      omp_ranks = 3
+    elif [ "$ranks" -eq "28"]
+    then
+      mpi_ranks = 7
+      omp_ranks = 4
+    elif [ "$ranks" -eq "29"]
+    then
+      mpi_ranks = 29
+      omp_ranks = 1
+    elif [ "$ranks" -eq "30"]
+    then
+      mpi_ranks = 6
+      omp_ranks = 5
+    elif [ "$ranks" -eq "31"]
+    then
+      mpi_ranks = 31
+      omp_ranks = 1
+    elif [ "$ranks" -eq "32"]
+    then
+      mpi_ranks = 8
+      omp_ranks = 4
+    elif [ "$ranks" -eq "33"]
+    then
+      mpi_ranks = 11
+      omp_ranks = 3
+    elif [ "$ranks" -eq "34"]
+    then
+      mpi_ranks = 17
+      omp_ranks = 2
+    elif [ "$ranks" -eq "35"]
+    then
+      mpi_ranks = 7
+      omp_ranks = 5
+    elif [ "$ranks" -eq "36"]
+    then
+      mpi_ranks = 9
+      omp_ranks = 4
+    fi
 
-
-  #   bsub -We 01:00 -n $reserve -J "mpi_strong[$ranks]%36" -R "span[ptile=24]" -R "rusage[mem=$mem]" -R "select[model==$model]" -oo "output_$ranks.txt" mpirun -n $ranks ../../bin/lu-mpi $runs $N
-  # fi
+    export OMP_NUM_THREADS=$omp_ranks
+    bsub -We 01:00 -n $ranks -J "lu_hybrid_strong[$ranks]%36" -R "span[ptile=$omp_ranks]" -R "rusage[mem=$mem]" -R "select[model==$model]" -oo "output_$ranks.txt" mpirun -n $mpi_ranks ../../bin/lu-hybrid $runs $N
+  fi
 
   if [ "$1" = "scalapack" ] || [ "$1" = "all" ]
   then
